095b6cdc154707f40d30612e3c3b13bdc981f9a9
Marco Cesarato <cesarato.developer@gmail.com>
2018-06-02 13:21:56 +0200

    First commit

---
+++ CPDO.class.php
@@ -0,0 +1,292 @@
+<?php
+
+/**
+ * CPDO
+ * Memory Cached PDO Class
+ * @author Marco Cesarato <cesarato.developer@gmail.com>
+ * @copyright Copyright (c) 2018
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
+ * @link https://github.com/marcocesarato/CPDO
+ * @version 0.1.1
+ */
+class CPDO extends PDO
+{
+
+    /**
+     * CPDO constructor.
+     */
+    function __construct($dsn, $username = "", $password = "", $driver_options = array())
+    {
+        parent::__construct($dsn, $username, $password, $driver_options);
+        $this->setAttribute(PDO::ATTR_STATEMENT_CLASS, array('CPDOStatement', array($this)));
+    }
+
+    /**
+     * Autogeneration of the DSN
+     * @param $database_type
+     * @param $database_name
+     * @param null $database_host
+     * @param null $database_user
+     * @param null $database_pswd
+     * @return bool|static
+     */
+    public static function connect($database_type, $database_name, $database_host = null, $database_user = null, $database_pswd = null){
+        if ($database_type == 'mysql') {
+            return new static('mysql:host='.$database_host.';dbname='.$database_name, $database_user, $database_pswd);
+        } elseif ($database_type == 'pgsql') {
+            return new static('pgsql:host='.$database_host.';dbname='.$database_name, $database_user, $database_pswd);
+        } elseif ($database_type == 'mssql') {
+            return new static('sqlsrv:Server='.$database_host.';Database='.$database_name, $database_user, $database_pswd);
+        } elseif ($database_type == 'sqlite') {
+            return new static('sqlite:/'.$database_name);
+        } elseif ($database_type == 'oracle') {
+            return new static('oci:dbname='.$database_name);
+        } elseif ($database_type == 'ibm') {
+            return new static('ibm:DRIVER={IBM DB2 ODBC DRIVER};DATABASE='.$database_name.';HOSTNAME='.$database_host.';PROTOCOL=TCPIP;', $database_user, $database_pswd);
+        } elseif (($database_type == 'firebird') || ($database_type == 'interbase')) {
+            return new static('firebird:dbname='.$database_name.';host='.$database_host);
+        } elseif ($database_type == '4D') {
+            return new static('4D:host='.$database_host, $database_user, $database_pswd);
+        } elseif ($database_type == 'informix') {
+            return new static('informix:host='.$database_host.'; database='.$database_name.'; server='.$database_host, $database_user, $database_pswd);
+        }
+        return false;
+    }
+
+}
+
+/**
+ * Class CPDOStatement
+ */
+class CPDOStatement extends PDOStatement
+{
+
+    public $queryString;
+    public static $__cache = array();
+
+    // From Light SQL Parser Class
+    protected static $__conts = array('OR', 'AND', 'ON', 'LIMIT', 'WHERE', 'JOIN', 'GROUP', 'ORDER', 'OPTION', 'LEFT', 'INNER', 'RIGHT', 'OUTER', 'SET', 'HAVING', 'VALUES', 'SELECT', '\(', '\)');
+    protected static $__conts_impl = '';
+
+    /**
+     * CachePDOStatement constructor.
+     */
+    protected function __construct() {
+        if(empty(self::$__conts_impl))
+            self::$__conts_impl = implode('|', self::$__conts);
+    }
+
+    /**
+     * @param null $input_parameters
+     * @return bool|void
+     */
+    public function execute($input_parameters = null)
+    {
+        $method = $this->__parseMethod();
+        switch ($method) {
+            case 'SELECT':
+                if (empty($this->__getcache())) {
+                    parent::execute($input_parameters);
+                }
+                break;
+            case 'INSERT':
+            case 'UPDATE':
+            case 'DELETE':
+            case 'DROP':
+            case 'TRUNCATE':
+                $this->__deletecache();
+                parent::execute($input_parameters);
+                break;
+            default:
+                parent::execute($input_parameters);
+        }
+    }
+
+    /**
+     * @param null $fetch_style
+     * @param int $cursor_orientation
+     * @param int $cursor_offset
+     * @return mixed
+     */
+    public function fetch($fetch_style = null, $cursor_orientation = PDO::FETCH_ORI_NEXT, $cursor_offset = 0)
+    {
+        $cache = $this->__getcache($fetch_style);
+        if (empty($cache)) {
+            $result = parent::fetch($fetch_style/*, $cursor_orientation, $cursor_offset*/);
+            $this->__setcache($result, $fetch_style);
+            return $result;
+        }
+        return $cache;
+    }
+
+    /**
+     * @param null $fetch_style
+     * @param null $fetch_argument
+     * @param array $ctor_args
+     * @return array
+     */
+    public function fetchAll($fetch_style = null, $fetch_argument = null, $ctor_args = array())
+    {
+        $cache = $this->__getcache($fetch_style);
+        if (empty($cache)) {
+            $result = parent::fetchAll($fetch_style/*, $fetch_argument, $ctor_args*/);
+            $this->__setcache($result, 'all_' . $fetch_style);
+            return $result;
+        }
+        return $cache;
+    }
+
+    /**
+     * @param string $class_name
+     * @param array $ctor_args
+     * @return mixed
+     */
+    public function fetchObject($class_name = "stdClass", $ctor_args = array())
+    {
+        $cache = $this->__getcache($class_name);
+        if (empty($cache)) {
+            $result = parent::fetchObject($class_name/*, $ctor_args*/);
+            $this->__setcache($result, $class_name);
+            return $result;
+        }
+        return $cache;
+    }
+
+    /**
+     * @param int $column_number
+     * @return mixed
+     */
+    public function fetchColumn($column_number = 0)
+    {
+        $cache = $this->__getcache($column_number);
+        if (empty($cache)) {
+            $result = parent::fetchColumn($column_number);
+            $this->__setcache($result, $column_number);
+            return $result;
+        }
+        return $cache;
+    }
+
+    /**
+     * Set Cache
+     * @param $value
+     */
+    protected function __setcache($value, $arg = null)
+    {
+        $e = new \Exception();
+        $trace = $e->getTrace();
+        $function = $trace[1]['function'];
+
+        $table = $this->__parseTable();
+        self::$__cache[$table][$this->queryString][$function][$arg] = $value;
+    }
+
+    /**
+     * Get Cache
+     * @return mixed
+     */
+    protected function __getcache($arg = null)
+    {
+        $e = new \Exception();
+        $trace = $e->getTrace();
+        $function = $trace[1]['function'];
+
+        $table = $this->__parseTable();
+        if (isset(self::$__cache[$table][$this->queryString][$function][$arg]))
+            return self::$__cache[$table][$this->queryString][$function][$arg];
+        return null;
+    }
+
+    /**
+     * Delete Cache
+     */
+    protected function __deletecache()
+    {
+        $table = $this->__parseTable();
+        self::$__cache[$table] = array();
+    }
+
+
+    /**
+     * Get SQL Query method
+     * @package Light-SQL-Parser-Class
+     * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
+     *
+     * @param $query
+     * @return string
+     */
+    protected function __parseMethod(){
+        $methods = array('SELECT','INSERT','UPDATE','DELETE','RENAME','SHOW','SET','DROP','CREATE INDEX','CREATE TABLE','EXPLAIN','DESCRIBE','TRUNCATE','ALTER');
+        $queries = $this->__parseQueries();
+        foreach($queries as $query){
+            foreach($methods as $method) {
+                $_method = str_replace(' ', '[\s]+', $method);
+                if(preg_match('#^[\s]*'.$_method.'[\s]+#i', $query)){
+                    return $method;
+                }
+            }
+        }
+        return '';
+    }
+
+    /**
+     * Get SQL Query First Table
+     * @package Light-SQL-Parser-Class
+     * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
+     *
+     * @param $query
+     * @return string
+     */
+    public function __parseTable(){
+        $tables = $this->__parseTables();
+        return (isset($tables[0])) ? $tables[0] : null;
+    }
+
+    /**
+     * Get SQL Query Tables
+     * @package Light-SQL-Parser-Class
+     * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
+     *
+     * @return array
+     */
+    protected function __parseTables(){
+        $results = array();
+        $queries = $this->__parseQueries();
+        foreach($queries as $query) {
+            $patterns = array(
+                '#[\s]+FROM[\s]+(([\s]*(?!'.self::$__conts_impl.')[\w]+([\s]+(AS[\s]+)?(?!'.self::$__conts_impl.')[\w]+)?[\s]*[,]?)+)#i',
+                '#[\s]*INSERT[\s]+INTO[\s]+([\w]+)#i',
+                '#[\s]*UPDATE[\s]+([\w]+)#i',
+                '#[\s]+[\s]+JOIN[\s]+([\w]+)#i',
+                '#[\s]+TABLE[\s]+([\w]+)#i'
+            );
+            foreach($patterns as $pattern){
+                preg_match_all($pattern,$query, $matches, PREG_SET_ORDER);
+                foreach ($matches as $val) {
+                    $tables = explode(',', $val[1]);
+                    foreach ($tables as $table) {
+                        $table = trim(preg_replace('#[\s]+(AS[\s]+)[\w\.]+#i', '', $table));
+                        $results[] = $table;
+                    }
+                }
+            }
+        }
+        return array_unique($results);
+    }
+
+    /**
+     * Get SQL Query method
+     * @package Light-SQL-Parser-Class
+     * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
+     *
+     * Get all queries
+     * @return array
+     */
+    protected function __parseQueries(){
+        $queries = preg_replace('#\/\*[\s\S]*?\*\/#','', $this->queryString);
+        $queries = preg_replace('#;(?:(?<=["\'];)|(?=["\']))#', '', $queries);
+        $queries = preg_replace('#[\s]*UNION([\s]+ALL)?[\s]*#', ';', $queries);
+        $queries = explode(';', $queries);
+        return $queries;
+    }
+}
---
+++ README.md
@@ -0,0 +1,37 @@
+# CPDO - Memory Cached PDO Class
+Version: 0.1
+
+This class permit to have cached `SELECT` queries on Memory (RAM). Then after the execution the cache will be deleted.
+
+Cache is cleaned on `INSERT/UPDATE/DELETE/TRUNCATE...` only for the single table.
+
+## What problem this solves
+When we call the same query (for example on ORM based system) we retrieve from the database the same data doing the same operation and some time we overload the database (for example retrieving big data multiple times).
+
+This class prevent to do the same query on the database, retrieving the data from memroy without overload the Database in some cases.
+
+### Requirements
+* PHP
+* Database
+
+## Databases supported (`CDPO::connect`)
+* 4D
+* CUBRID
+* Firebird/Interbase
+* IBM
+* Informix
+* MS SQL Server
+* MySQL
+* ODBC and DB2
+* Oracle
+* PostgreSQL
+* SQLite
+
+## Usage
+You can use this class as PDO.
+
+The only one new feature implemented in this class is the DSN autogeneration if you use the method 
+
+`CPDO::connect($database_type, $database_name, $database_host = null, $database_user = null, $database_pswd = null)` 
+
+instead use the constructor.

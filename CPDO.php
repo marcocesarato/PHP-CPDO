7b61c065988ef7777625bb887f5f354dadc8d8d1
Marco Cesarato <marco.cesarato@eniac.it>
2018-06-05 17:28:23 +0200

    Added exec and query methods and moved to CDPOCache

--- CPDO.php
+++ CPDO.php
@@ -7,11 +7,10 @@
  * @copyright Copyright (c) 2018
  * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License
  * @link https://github.com/marcocesarato/CPDO
- * @version 0.1.2.11
+ * @version 0.1.3.12
  */
 class CPDO extends PDO
 {
-
     /**
      * CPDO constructor.
      */
@@ -21,15 +20,15 @@ class CPDO extends PDO
         $this->setAttribute(PDO::ATTR_STATEMENT_CLASS, array('CPDOStatement', array($this)));
     }
 
-    /**
-     * Autogeneration of the DSN
-     * @param $database_type
-     * @param null $database_name
-     * @param null $database_host
-     * @param null $database_user
-     * @param null $database_pswd
-     * @return bool|static
-     */
+	/**
+	 * Autogeneration of the DSN
+	 * @param $database_type
+	 * @param null $database_name
+	 * @param null $database_host
+	 * @param null $database_user
+	 * @param null $database_pswd
+	 * @return bool|static
+	 */
     public static function connect($database_type, $database_name = null, $database_host = null, $database_user = null, $database_pswd = null){
 
     	if(empty($database_host) && in_array($database_type, array('mysql', 'pgsql', 'mssql', 'ibm', 'firebird', '4D', 'interbase', 'informix'))){
@@ -68,6 +67,69 @@ class CPDO extends PDO
         return false;
     }
 
+	/**
+	 * @param string $statement
+	 * @return bool|int|null
+	 */
+	public function exec ($statement) {
+		$method = CPDOCache::parseMethod($statement);
+		switch ($method) {
+			case 'SELECT':
+			case 'SHOW':
+			case 'DESCRIBE':
+				$cache = CPDOCache::getcache($statement);
+				if (empty($cache)) {
+					$result = parent::exec($statement);
+					CPDOCache::setcache($statement, $result, 'exec_');
+					return $result;
+				}
+				return $cache;
+				break;
+			case 'INSERT':
+			case 'UPDATE':
+			case 'DELETE':
+			case 'DROP':
+			case 'TRUNCATE':
+			case 'ALTER':
+				CPDOCache::deletecache($statement);
+			default:
+				return parent::exec($statement);
+		}
+	}
+
+	/**
+	 * @param string $statement
+	 * @param int $mode
+	 * @param null $arg3
+	 * @param array $ctorargs
+	 * @return array|bool|null|PDOStatement
+	 */
+	public function query($statement, $mode = PDO::ATTR_DEFAULT_FETCH_MODE, $arg3 = null, array $ctorargs = array()) {
+		$method = CPDOCache::parseMethod($statement);
+		switch ($method) {
+			case 'SELECT':
+			case 'SHOW':
+			case 'DESCRIBE':
+				$cache = CPDOCache::getcache($statement);
+				if (empty($cache)) {
+					$result = parent::query($statement, $mode/*,$arg3, $ctorargs*/);
+					CPDOCache::setcache($statement, $result, 'query_'.$mode);
+					return $result;
+				}
+				return $cache;
+				break;
+			case 'INSERT':
+			case 'UPDATE':
+			case 'DELETE':
+			case 'DROP':
+			case 'TRUNCATE':
+			case 'ALTER':
+				CPDOCache::deletecache($statement);
+			default:
+				return parent::query($statement, $mode/*,$arg3, $ctorargs*/);
+		}
+	}
+
 }
 
 /**
@@ -77,24 +139,20 @@ class CPDOStatement extends PDOStatement
 {
 
     public $queryString;
-    public static $__cache = array();
 
-    // From Light SQL Parser Class
-    protected static $__parser_method = array();
-    protected static $__parser_tables = array();
-
-    /**
-     * @param null $input_parameters
-     * @return bool|void
-     */
+	/**
+	 * @param null $input_parameters
+	 * @return bool|void
+	 */
     public function execute($input_parameters = null)
     {
-        $method = $this->__parseMethod();
+        $method = CPDOCache::parseMethod($this->queryString);
+	    $cache = CPDOCache::getcache($this->queryString);
         switch ($method) {
             case 'SELECT':
             case 'SHOW':
             case 'DESCRIBE':
-                if (empty($this->__getcache())) {
+                if (empty($cache)) {
                     parent::execute($input_parameters);
                 }
                 break;
@@ -104,214 +162,228 @@ class CPDOStatement extends PDOStatement
             case 'DROP':
             case 'TRUNCATE':
             case 'ALTER':
-                $this->__deletecache();
-                parent::execute($input_parameters);
-                break;
+	            CPDOCache::deletecache($this->queryString);
             default:
                 parent::execute($input_parameters);
         }
     }
 
-    /**
-     * @param null $fetch_style
-     * @param int $cursor_orientation
-     * @param int $cursor_offset
-     * @return mixed
-     */
+	/**
+	 * @param null $fetch_style
+	 * @param int $cursor_orientation
+	 * @param int $cursor_offset
+	 * @return mixed|null
+	 */
     public function fetch($fetch_style = null, $cursor_orientation = PDO::FETCH_ORI_NEXT, $cursor_offset = 0)
     {
-        $cache = $this->__getcache($fetch_style);
+        $cache = CPDOCache::getcache($this->queryString,$fetch_style);
         if (empty($cache)) {
-            $result = parent::fetch($fetch_style/*, $cursor_orientation, $cursor_offset*/);
-            $this->__setcache($result, $fetch_style);
+            $result = parent::fetch($this->queryString,$fetch_style/*, $cursor_orientation, $cursor_offset*/);
+	        CPDOCache::setcache($this->queryString, $result, $fetch_style);
             return $result;
         }
         return $cache;
     }
 
-    /**
-     * @param null $fetch_style
-     * @param null $fetch_argument
-     * @param array $ctor_args
-     * @return array
-     */
+	/**
+	 * @param null $fetch_style
+	 * @param null $fetch_argument
+	 * @param array $ctor_args
+	 * @return array|null
+	 */
     public function fetchAll($fetch_style = null, $fetch_argument = null, $ctor_args = array())
     {
-        $cache = $this->__getcache($fetch_style);
+        $cache = CPDOCache::getcache($this->queryString, $fetch_style);
         if (empty($cache)) {
             $result = parent::fetchAll($fetch_style/*, $fetch_argument, $ctor_args*/);
-            $this->__setcache($result, 'all_' . $fetch_style);
+	        CPDOCache::setcache($this->queryString, $result, 'all_' . $fetch_style);
             return $result;
         }
         return $cache;
     }
 
-    /**
-     * @param string $class_name
-     * @param array $ctor_args
-     * @return mixed
-     */
+	/**
+	 * @param string $class_name
+	 * @param array $ctor_args
+	 * @return mixed|null
+	 */
     public function fetchObject($class_name = "stdClass", $ctor_args = array())
     {
-        $cache = $this->__getcache($class_name);
+        $cache = CPDOCache::getcache($this->queryString, $class_name);
         if (empty($cache)) {
             $result = parent::fetchObject($class_name/*, $ctor_args*/);
-            $this->__setcache($result, $class_name);
+	        CPDOCache::setcache($this->queryString, $result, $class_name);
             return $result;
         }
         return $cache;
     }
 
-    /**
-     * @param int $column_number
-     * @return mixed
-     */
+	/**
+	 * @param int $column_number
+	 * @return mixed|null
+	 */
     public function fetchColumn($column_number = 0)
     {
-        $cache = $this->__getcache($column_number);
+        $cache = CPDOCache::getcache($this->queryString, $column_number);
         if (empty($cache)) {
             $result = parent::fetchColumn($column_number);
-            $this->__setcache($result, $column_number);
+	        CPDOCache::setcache($this->queryString, $result, $column_number);
             return $result;
         }
         return $cache;
     }
+}
 
-    /**
-     * Set Cache
-     * @param $value
-     */
-    protected function __setcache($value, $arg = null)
-    {
-        $e = new \Exception();
-        $trace = $e->getTrace();
-        $function = $trace[1]['function'];
+class CPDOCache
+{
 
-        $table = $this->__keycache();
-        self::$__cache[$table][$this->queryString][$function][$arg] = $value;
-    }
+	public static $__cache = array();
+	// From Light SQL Parser Class
+	protected static $__parser_method = array();
+	protected static $__parser_tables = array();
 
-    /**
-     * Get Cache
-     * @return mixed
-     */
-    protected function __getcache($arg = null)
-    {
-        $e = new \Exception();
-        $trace = $e->getTrace();
-        $function = $trace[1]['function'];
+	/**
+	 * Set Cache
+	 * @param $query
+	 * @param $value
+	 * @param null $arg
+	 */
+	public static function setcache($query, $value, $arg = null)
+	{
+		$e = new \Exception();
+		$trace = $e->getTrace();
+		$function = $trace[1]['function'];
 
-        $table = $this->__keycache();
-        if (isset(self::$__cache[$table][$this->queryString][$function][$arg]))
-            return self::$__cache[$table][$this->queryString][$function][$arg];
-        return null;
-    }
+		$table = self::keycache($query);
+		self::$__cache[$table][$query][$function][$arg] = $value;
+	}
 
-    /**
-     * Delete Cache
-     */
-    protected function __deletecache()
-    {
-        $tables = $this->__parseTables();
-        foreach (array_keys(self::$__cache) as $key){
-            foreach ($tables as $table){
-                if(strpos($key, $table) !== false ) {
-                    self::$__cache[$key] = array();
-                }
-            }
-        }
-    }
+	/**
+	 * Get Cache
+	 * @param $query
+	 * @param null $arg
+	 * @return null
+	 */
+	public static function getcache($query, $arg = null)
+	{
+		$e = new \Exception();
+		$trace = $e->getTrace();
+		$function = $trace[1]['function'];
 
-    /**
-     * Get key cache
-     * @param $query
-     * @return string
-     */
-    protected function __keycache(){
-        $tables = $this->__parseTables();
-        return implode('/',$tables);
-    }
+		$table = self::keycache($query);
+		if (isset(self::$__cache[$table][$query][$function][$arg]))
+			return self::$__cache[$table][$query][$function][$arg];
+		return null;
+	}
 
-    /**
-     * Get SQL Query method
-     * @package Light-SQL-Parser-Class
-     * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
-     *
-     * @param $query
-     * @return string
-     */
-    protected function __parseMethod(){
+	/**
+	 * Delete Cache
+	 * @param $query
+	 */
+	public static function deletecache($query)
+	{
+		$tables = self::parseTables($query);
+		foreach (array_keys(self::$__cache) as $key){
+			foreach ($tables as $table){
+				if(strpos($key, $table) !== false ) {
+					self::$__cache[$key] = array();
+				}
+			}
+		}
+	}
 
-        if(!empty(self::$__parser_method[$this->queryString]))
-            return self::$__parser_method[$this->queryString];
+	/**
+	 * Get key cache
+	 * @param $query
+	 * @return string
+	 */
+	public static function keycache($query){
+		$tables = self::parseTables($query);
+		return implode('/',$tables);
+	}
 
-        $methods = array('SELECT','INSERT','UPDATE','DELETE','RENAME','SHOW','SET','DROP','CREATE INDEX','CREATE TABLE','EXPLAIN','DESCRIBE','TRUNCATE','ALTER');
-        $queries = $this->__parseQueries();
-        foreach($queries as $query){
-            foreach($methods as $method) {
-                $_method = str_replace(' ', '[\s]+', $method);
-                if(preg_match('#^[\s]*'.$_method.'[\s]+#i', $query)){
-                    self::$__parser_method[$this->queryString] = $method;
-                    return $method;
-                }
-            }
-        }
-        return '';
-    }
+	/**
+	 * Get SQL Query method
+	 * @package Light-SQL-Parser-Class
+	 * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
+	 *
+	 * @param $query
+	 * @return mixed|string
+	 */
+	public static function parseMethod($query){
 
-    /**
-     * Get SQL Query Tables
-     * @package Light-SQL-Parser-Class
-     * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
-     *
-     * @return array
-     */
-    protected function __parseTables(){
+		if(!empty(self::$__parser_method[$query]))
+			return self::$__parser_method[$query];
 
-	    $connectors = "OR|AND|ON|LIMIT|WHERE|JOIN|GROUP|ORDER|OPTION|LEFT|INNER|RIGHT|OUTER|SET|HAVING|VALUES|SELECT|\(|\)";
+		$methods = array('SELECT','INSERT','UPDATE','DELETE','RENAME','SHOW','SET','DROP','CREATE INDEX','CREATE TABLE','EXPLAIN','DESCRIBE','TRUNCATE','ALTER');
+		$queries = self::parseQueries($query);
+		foreach($queries as $query){
+			foreach($methods as $method) {
+				$_method = str_replace(' ', '[\s]+', $method);
+				if(preg_match('#^[\s]*'.$_method.'[\s]+#i', $query)){
+					self::$__parser_method[$query] = $method;
+					return $method;
+				}
+			}
+		}
+		return '';
+	}
 
-        if(!empty(self::$__parser_tables[$this->queryString]))
-            return self::$__parser_tables[$this->queryString];
+	/**
+	 * Get SQL Query Tables
+	 * @package Light-SQL-Parser-Class
+	 * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
+	 *
+	 * @param $_query
+	 * @return array|mixed
+	 */
+	public static function parseTables($_query){
 
-        $results = array();
-        $queries = $this->__parseQueries();
-        foreach($queries as $query) {
-            $patterns = array(
-                '#[\s]+FROM[\s]+(([\s]*(?!'.$connectors.')[\w]+([\s]+(AS[\s]+)?(?!'.$connectors.')[\w]+)?[\s]*[,]?)+)#i',
-                '#[\s]*INSERT[\s]+INTO[\s]+([\w]+)#i',
-                '#[\s]*UPDATE[\s]+([\w]+)#i',
-                '#[\s]+[\s]+JOIN[\s]+([\w]+)#i',
-                '#[\s]+TABLE[\s]+([\w]+)#i'
-            );
-            foreach($patterns as $pattern){
-                preg_match_all($pattern,$query, $matches, PREG_SET_ORDER);
-                foreach ($matches as $val) {
-                    $tables = explode(',', $val[1]);
-                    foreach ($tables as $table) {
-                        $table = trim(preg_replace('#[\s]+(AS[\s]+)[\w\.]+#i', '', $table));
-                        $results[] = $table;
-                    }
-                }
-            }
-        }
-        $tables = array_unique($results);
-        self::$__parser_tables[$this->queryString] = $tables;
-        return $tables;
-    }
+		$connectors = "OR|AND|ON|LIMIT|WHERE|JOIN|GROUP|ORDER|OPTION|LEFT|INNER|RIGHT|OUTER|SET|HAVING|VALUES|SELECT|\(|\)";
 
-    /**
-     * Get SQL Query method
-     * @package Light-SQL-Parser-Class
-     * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
-     *
-     * Get all queries
-     * @return array
-     */
-    protected function __parseQueries(){
-        $queries = preg_replace('#\/\*[\s\S]*?\*\/#','', $this->queryString);
-        $queries = preg_replace('#;(?:(?<=["\'];)|(?=["\']))#', '', $queries);
-        $queries = preg_replace('#[\s]*UNION([\s]+ALL)?[\s]*#', ';', $queries);
-        $queries = explode(';', $queries);
-        return $queries;
-    }
+		if(!empty(self::$__parser_tables[$_query]))
+			return self::$__parser_tables[$_query];
+
+		$results = array();
+		$queries = self::parseQueries($_query);
+		foreach($queries as $query) {
+			$patterns = array(
+				'#[\s]+FROM[\s]+(([\s]*(?!'.$connectors.')[\w]+([\s]+(AS[\s]+)?(?!'.$connectors.')[\w]+)?[\s]*[,]?)+)#i',
+				'#[\s]*INSERT[\s]+INTO[\s]+([\w]+)#i',
+				'#[\s]*UPDATE[\s]+([\w]+)#i',
+				'#[\s]+[\s]+JOIN[\s]+([\w]+)#i',
+				'#[\s]+TABLE[\s]+([\w]+)#i'
+			);
+			foreach($patterns as $pattern){
+				preg_match_all($pattern,$query, $matches, PREG_SET_ORDER);
+				foreach ($matches as $val) {
+					$tables = explode(',', $val[1]);
+					foreach ($tables as $table) {
+						$table = trim(preg_replace('#[\s]+(AS[\s]+)[\w\.]+#i', '', $table));
+						$results[] = $table;
+					}
+				}
+			}
+		}
+		$tables = array_unique($results);
+		self::$__parser_tables[$_query] = $tables;
+		return $tables;
+	}
+
+	/**
+	 * Get SQL Query method
+	 * @package Light-SQL-Parser-Class
+	 * @link https://github.com/marcocesarato/PHP-Light-SQL-Parser-Class
+	 *
+	 * Get all queries
+	 * @param $query
+	 * @return array|null|string|string[]
+	 */
+	public static function parseQueries($query){
+		$queries = preg_replace('#\/\*[\s\S]*?\*\/#','', $query);
+		$queries = preg_replace('#;(?:(?<=["\'];)|(?=["\']))#', '', $queries);
+		$queries = preg_replace('#[\s]*UNION([\s]+ALL)?[\s]*#', ';', $queries);
+		$queries = explode(';', $queries);
+		return $queries;
+	}
 }
